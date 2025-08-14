8bf4af896753a8e79264bacf56df83d6b17a068e
Victor Rocha Alves <victor.rocha.alves396@gmail.com>
2025-08-14 15:26:20 -0300

    Add files via upload

---
+++ csharp/csharp1/csharp1/Program.cs
@@ -0,0 +1,44 @@
+﻿double N1 = 0;
+double N2 = 0;
+
+while (true)
+{
+    Console.WriteLine("Entre com a nota da N1: ");
+    N1 = Convert.ToDouble(Console.ReadLine());
+    if(N1 >= 0 && N1 <= 10)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("\nEntrou com uma nota inválida!");
+    }
+}
+while (true)
+{
+    Console.WriteLine("\nEntre com a nota da N2: ");
+    N2 = Convert.ToDouble(Console.ReadLine());
+    if (N2 >= 0 && N2 <= 10)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("\nEntrou com uma nota inválida!");
+    }
+}
+
+double media = (N1 + N2) / 2;
+
+if (media >= 7 && media <= 10)
+{
+    Console.WriteLine($"\nSua média foi: {media}. \nVocê está APROVADO!!");
+}
+else if (media < 7 && media > 3)
+{
+    Console.WriteLine($"\nSua média foi: {media}. \nVocê está de final!!");
+}
+else if (media < 3)
+{
+    Console.WriteLine($"\nSua média foi: {media}. \nVocê está REPROVADO!!");
+}
---
+++ csharp/csharp2/csharp2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Entre com a sua idade: ");
+double idade = Convert.ToDouble(Console.ReadLine());
+
+if (idade < 12)
+{
+    Console.WriteLine("\nVocê é uma CRIANÇA!");
+}
+else if(idade >= 12 && idade < 18)
+{
+    Console.WriteLine("\nVocê é um ADOLESCENTE!");
+}
+else if(idade >= 18)
+{
+    Console.WriteLine("\nVocê é um ADULTO!");
+}
+
